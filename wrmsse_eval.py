6246e384a63c7a2c036243fe7eded6172e2871ec
Oksana <oksanabolibok496@gmail.com>
2024-07-08 04:32:30 +0400

    Refactoring  wrmsse_eval.py

--- wrmsse_eval.py
+++ wrmsse_eval.py
@@ -65,38 +65,7 @@ def compute_weights(train_df,valid_df,weight_cols,groups,fix_cols, calendar_df, 
     weights = pd.DataFrame(weights_map).T / len(groups) #creating a dataframe with weights corresponding to each group keys of 42840 hierachical time-series
     return weights
 
-# def compute_weights(train_df, weight_cols, groups, fix_cols, calendar_df, prices_df):
-#     """
-#     Computes the weights for each of the 42840 series using the last 28 days
-#     of sales and their prices.
-#
-#     Parameters:
-#     train_df (pd.DataFrame): Training dataset.
-#     weight_cols (list): Columns representing the last 28 days of data.
-#     groups (list): Grouping keys for the hierarchical levels.
-#     fix_cols (list): Fixed identifier columns.
-#     calendar_df (pd.DataFrame): Calendar dataframe.
-#     prices_df (pd.DataFrame): Prices dataframe.
-#
-#     Returns:
-#     pd.DataFrame: Weights for the 42840 series.
-#     """
-#     weights_map = {}
-#     weight_df = train_df[["item_id", "store_id"] + weight_cols]
-#     weight_df = weight_df.melt(id_vars=["item_id", "store_id"], var_name='d', value_name='sales')
-#     weight_df = weight_df.merge(calendar_df[['wm_yr_wk', 'd']], on='d', how='left')
-#     weight_df = weight_df.merge(prices_df, on=["item_id", "store_id", "wm_yr_wk"])
-#     weight_df["dollar_sales"] = weight_df["sales"] * weight_df["sell_price"]
-#     weight_df = weight_df.set_index(["item_id", "store_id", "d"]).unstack(level=2)["dollar_sales"]
-#     weight_df = weight_df.loc[zip(train_df.item_id, train_df.store_id), :].reset_index(drop=True)
-#     weight_df = pd.concat([train_df[fix_cols], weight_df], axis=1, sort=False)
-#
-#     for i,grp in enumerate(groups):
-#         ser_weight = weight_df.groupby(grp)[weight_cols].sum().sum(axis=1)
-#         ser_weight = ser_weight / ser_weight.sum()
-#         for j in range(len(ser_weight)):
-#             weights_map[gen_series_name(ser_weight.index[j])] = np.array([ser_weight.iloc[j]])
-#     return pd.DataFrame(weights_map).T / len(groups)
+
 
 def compute_rmsse(train_df, valid_df, pred_df):
     """
